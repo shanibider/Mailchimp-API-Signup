81419f031842848a9077fd5a076c788af01c8aae
shanibider <shanibider@gmail.com>
2024-03-08 18:59:07 +0200

    first commit

--- app.js
+++ app.js
@@ -1,79 +1,93 @@
+// https://mailchimp.com/developer/marketing/docs/fundamentals/
+// https://mailchimp.com/developer/marketing/api/ecommerce-orders/
+// https://mailchimp.com/developer/marketing/api/lists/
+// https://mailchimp.com/developer/marketing/guides/access-user-data-oauth-2/?_ga=2.230154139.609880322.1709910708-1755440482.1709910707
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
 const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
-  const firstName= req.body.fName;
-  const lastName= req.body.lName;
-  const email= req.body.email;
+// Simplified object destructuring to directly extract fName, lName, and email
+app.post("/", function(req, res) {
+  const { fName, lName, email } = req.body;
 
-//data= JS object
-var data =  {
-  members: [
+
+// will need to send JSON object.
+//firstly, create javascript object. With key, value pairs that mailchimp is expecting.
+const data =  {
+  members: [        //array of objects
     {
-      email_address: email,
+      email_address: email,       // the email from the post request (the form)
       status: "subscribed",
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName
+      merge_fields: {             //from admin.mailchimp.com/lists/settings/merge-tags 
+        FNAME: fName,
+        LNAME: lName
       }
     }
   ]
 };
 
-const jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);  //convert the javascript object to a JSON string, that mailchimp is expecting.
 
-const url = "https://us21.api.mailchimp.com/3.0/lists/9df456b62d";
+  const url = "https://us21.api.mailchimp.com/3.0/lists/9df456b62d";
 
-const options= {
+  // options for my http post request 
+  const options = {
     method: "POST",
     auth: "shani:f2da992f94f4a864713b69f0928132e0-us21"
-};
+  };
 
-const request = https.request(url, options, function(response){
+  // Post Request to mailchimp server (url, options above, callback function from mailchimp server)
+  const mailchimpRequest = https.request(url, options, function(response) {
+    let responseData = "";
 
-if (response.statusCode === 200){
-  res.sendFile(__dirname + "/success.html");
-} else {
-  res.sendFile(__dirname + "/failure.html");
-}
+    response.on("data", function(chunk) {
+      responseData += chunk;
+    });
 
-  response.on("data", function(data){
-    console.log(JSON.parse(data));
+    response.on("end", function() {
+      const responseDataObject = JSON.parse(responseData);
+
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        console.error("Mailchimp API Error:", responseDataObject);
+        res.sendFile(__dirname + "/failure.html");
+      }
+    });
   });
+
+  mailchimpRequest.on("error", function(error) {
+    console.error("Mailchimp API Request Error:", error);
+    res.sendFile(__dirname + "/error.html");
+  });
+
+  mailchimpRequest.write(jsonData);
+  mailchimpRequest.end();
 });
 
-//request.write(jsonData);
-request.end();
-});
-
-
-
-app.post("/failure", function(req,res){
+// post request to catch the failure route
+app.post("/failure", function(req, res) {
   res.redirect("/");
 });
 
-
-
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("server is running on port 3000");
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
 });
 
 
-//API key
+
+//mailchinpAPI key
 //f2da992f94f4a864713b69f0928132e0-us21
 
-//List Id
+//List Id - Unique Audience ID for: shani -
 //9df456b62d
